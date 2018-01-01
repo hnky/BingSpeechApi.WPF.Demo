5e1c89dc6aed9428a01137b0cbff29fa6342af4a
Henk Boelman <henk@boelman.eu>
2018-01-01 13:38:17 +0100

    Init

---
+++ BingSpeechDemo/MainWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.CognitiveServices.SpeechRecognition;
+
+namespace BingSpeechDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private MicrophoneRecognitionClient micClient;
+
+        private string DefaultLocale = "en-US";
+
+        private string SubscriptionKey = "";
+
+        private string AuthenticationUri = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.CreateMicrophoneRecoClient();
+            this.micClient.StartMicAndRecognition();
+        }
+
+
+
+        private void CreateMicrophoneRecoClient()
+        {
+            this.micClient = SpeechRecognitionServiceFactory.CreateMicrophoneClient(
+                SpeechRecognitionMode.LongDictation,
+                this.DefaultLocale,
+                this.SubscriptionKey);
+            this.micClient.AuthenticationUri = this.AuthenticationUri;
+
+            // Event handlers for speech recognition results
+            this.micClient.OnMicrophoneStatus += this.OnMicrophoneStatus;
+
+            this.micClient.OnPartialResponseReceived += this.OnPartialResponseReceivedHandler;
+
+            this.micClient.OnResponseReceived += this.OnMicDictationResponseReceivedHandler;
+
+            this.micClient.OnConversationError += this.OnConversationErrorHandler;
+        }
+
+
+        /// <summary>
+        /// Called when the microphone status has changed.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="MicrophoneEventArgs"/> instance containing the event data.</param>
+        private void OnMicrophoneStatus(object sender, MicrophoneEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                WriteLine("--- Microphone status change received by OnMicrophoneStatus() ---");
+                WriteLine("********* Microphone status: {0} *********", e.Recording);
+                if (e.Recording)
+                {
+                    WriteLine("Please start speaking.");
+                }
+
+                WriteLine();
+            });
+        }
+
+        /// <summary>
+        /// Called when a partial response is received.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="PartialSpeechResponseEventArgs"/> instance containing the event data.</param>
+        private void OnPartialResponseReceivedHandler(object sender, PartialSpeechResponseEventArgs e)
+        {
+            this.WriteLine("--- Partial result received by OnPartialResponseReceivedHandler() ---");
+            this.WriteLine("{0}", e.PartialResult);
+            this.WriteLine();
+        }
+
+        /// <summary>
+        /// Called when a final response is received;
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="SpeechResponseEventArgs"/> instance containing the event data.</param>
+        private void OnMicDictationResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
+        {
+            this.WriteLine("--- OnMicDictationResponseReceivedHandler ---");
+            if (e.PhraseResponse.RecognitionStatus == RecognitionStatus.EndOfDictation ||
+                e.PhraseResponse.RecognitionStatus == RecognitionStatus.DictationEndSilenceTimeout)
+            {
+                Dispatcher.Invoke(
+                    (Action)(() =>
+                    {
+                        // we got the final result, so it we can end the mic reco.  No need to do this
+                        // for dataReco, since we already called endAudio() on it as soon as we were done
+                        // sending all the data.
+                        this.micClient.EndMicAndRecognition();
+
+                      //  this._startButton.IsEnabled = true;
+                       // this._radioGroup.IsEnabled = true;
+                    }));
+            }
+
+            this.WriteResponseResult(e);
+        }
+
+        // <summary>
+        /// Called when an error is received.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="SpeechErrorEventArgs"/> instance containing the event data.</param>
+        private void OnConversationErrorHandler(object sender, SpeechErrorEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+              //  _startButton.IsEnabled = true;
+              //  _radioGroup.IsEnabled = true;
+            });
+
+            this.WriteLine("--- Error received by OnConversationErrorHandler() ---");
+            this.WriteLine("Error code: {0}", e.SpeechErrorCode.ToString());
+            this.WriteLine("Error text: {0}", e.SpeechErrorText);
+            this.WriteLine();
+        }
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// Writes the response result.
+        /// </summary>
+        /// <param name="e">The <see cref="SpeechResponseEventArgs"/> instance containing the event data.</param>
+        private void WriteResponseResult(SpeechResponseEventArgs e)
+        {
+            if (e.PhraseResponse.Results.Length == 0)
+            {
+                this.WriteLine("No phrase response is available.");
+            }
+            else
+            {
+                this.WriteLine("********* Final n-BEST Results *********");
+                for (int i = 0; i < e.PhraseResponse.Results.Length; i++)
+                {
+                    this.WriteLine(
+                        "[{0}] Confidence={1}, Text=\"{2}\"",
+                        i,
+                        e.PhraseResponse.Results[i].Confidence,
+                        e.PhraseResponse.Results[i].DisplayText);
+                }
+
+                this.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Writes the line.
+        /// </summary>
+        private void WriteLine()
+        {
+            this.WriteLine(string.Empty);
+        }
+
+        /// <summary>
+        /// Writes the line.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <param name="args">The arguments.</param>
+        private void WriteLine(string format, params object[] args)
+        {
+            var formattedStr = string.Format(format, args);
+            Trace.WriteLine(formattedStr);
+            Dispatcher.Invoke(() =>
+            {
+                _logText.Text += (formattedStr + "\n");
+                _logText.ScrollToEnd();
+            });
+        }
+
+
+
+    }
+}
